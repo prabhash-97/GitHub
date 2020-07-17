e18cdc9e9e37518ebd2d0998a1de43a13f49fd33
prabhash-97 <68389099+prabhash-97@users.noreply.github.com>
2020-07-17 15:30:51 +0530

    commit

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace GitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
